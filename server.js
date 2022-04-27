a5a81a0c9346defc665667685f88305b5eaf5600
Adam Sisk <adamsisk91@gmail.com>
2022-04-26 22:29:18 -0500

    Get Restaurants 2 solution

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const { Restaurant } = require('./models/Restaurant');
 const app = express();
 const PORT = 3000;
 
-// TODO 1: Create a new route for the restaurants
+/* GET /restaurants - return all restaurants */
 app.get('/restaurants', async (req, res) => {
   const restaurants = await Restaurant.findAll();
 
   res.json(restaurants);
 });
 
+/* GET /restaurants/:id - return 1 restaurant by id */
+app.get('/restaurants/:id', async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+
+  res.json(restaurant);
+});
+
 app.listen(PORT, () => {
   sequelize.sync();
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
