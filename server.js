9c4beb23ef3c59a1c2296bb5510e2f2582ff2853
Adam Sisk <adamsisk91@gmail.com>
2022-04-26 22:26:06 -0500

    ex-2 solution

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
 
-app.use(express.static('public'));
+/**
+ * @type {Array<{name: string, color: string}>}
+ */
+const animals = [
+  {
+    name: 'Zebra',
+    color: 'Mixed',
+  },
+  {
+    name: 'Lion',
+    color: 'Gold',
+  },
+  {
+    name: 'Tiger',
+    color: 'Orange',
+  },
+  {
+    name: 'Bear',
+    color: 'Brown',
+  },
+];
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+//Your code here
+
+app.get('/animals/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (+id > animals.length - 1) {
+    return res.sendStatus(404);
+  }
+
+  const animal = animals[+id];
+
+  res.json(animal);
+});
+
+//Do not edit below
+app.listen(3000, () => console.log('Listening on port 3000'));
